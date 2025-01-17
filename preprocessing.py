6f156db2c7fb6538e9ead5eae38c8db6025f9f92
Giacomor7 <giacomor7@gmail.com>
2025-01-17 13:46:23 +0000

    Add function to combine creatine results and dates into a single column

--- preprocessing.py
+++ preprocessing.py
@@ -7,6 +7,21 @@ def get_latest_result(data):
     :return:
     """
 
+def combine_result_and_time(data):
+    """
+    Combine each creatinine result and date into a single tuple
+    :param data: patient data as dataframe
+    :return: dataframe containing tuples for each result
+    """
+    creatinine_results = pd.DataFrame(
+        columns=[f"result_{i}" for i in range(44)])
+
+    for i in range(44):
+        creatinine_results[f"result_{i}"] = (
+        data[f"creatinine_result_{i}"], data[f"creatinine_date_{i}"])
+
+    return creatinine_results
+
 def extract_features(data):
     """
     Features to extract (taken from NHS algorithm):
@@ -23,6 +38,9 @@ def extract_features(data):
     :param data: dataframe containing patient data
     :return: dataframe containing features
     """
+
+    creatinine_results = combine_result_and_time(data)
+
     features = pd.DataFrame(
         columns=['age', 'sex', 'latest_result', 'previous_result', 'days_since_previous_result',
                  'past_48h_lowest_value', 'past_48h_variance', 'past_week_lowest_value', 'past_year_median'])
